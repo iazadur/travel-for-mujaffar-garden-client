07d412fd364c4891be01d3ac55c71f08a857bf7d
iazadur <iamazadur@gmail.com>
2021-10-30 13:20:14 +0600

    added services page

--- src/Pages/ServicePage/ServicePage.jsx
+++ src/Pages/ServicePage/ServicePage.jsx
@@ -1,73 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Heading from '../../Components/From/Heading';
 import Header from '../../Components/Header/Header';
 
 const ServicePage = () => {
-    const products = [
-        {
-            id: 1,
-            name: 'Basic Tee',
-            href: '#',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-            imageAlt: "Front of men's Basic Tee in black.",
-            price: '$35',
-            color: 'Black',
-        },
-        {
-            id: 2,
-            name: 'Basic Tee',
-            href: '#',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-            imageAlt: "Front of men's Basic Tee in black.",
-            price: '$35',
-            color: 'Black',
-        },
-        {
-            id: 3,
-            name: 'Basic Tee',
-            href: '#',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-            imageAlt: "Front of men's Basic Tee in black.",
-            price: '$35',
-            color: 'Black',
-        },
-        {
-            id: 4,
-            name: 'Basic Tee',
-            href: '#',
-            imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-            imageAlt: "Front of men's Basic Tee in black.",
-            price: '$35',
-            color: 'Black',
-        }
-    ]
+    const [services, setServices] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:5000/services')
+            .then(function (response) {
+                setServices(response.data)
+            })
+    }, [])
+    
     return (
         <>
         <Header></Header>
         <div className="bg-white">
             <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">Customers also purchased</h2>
+                <Heading text="Our Services"/>
 
                 <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {products.map((product) => (
-                        <div key={product.id} className="group relative">
+                    {services.map((service) => (
+                        <div key={service._id} className="group relative">
                             <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                                 <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
+                                    src={service.Image}
+                                    alt={service.Title}
                                     className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                                 />
                             </div>
-                            <div className="mt-4 flex justify-between">
+                            <div className="mt-4 flex justify-between" style={{color:`${service.btnColor}`}}>
                                 <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        <a href={product.href}>
+                                    <h3 className="text-sm ">
+                                    <Link to={`/bookNow/${service._id}`}>
                                             <span aria-hidden="true" className="absolute inset-0" />
-                                            {product.name}
-                                        </a>
+                                            {service.Title}
+                                        </Link>
                                     </h3>
-                                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                    
                                 </div>
-                                <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                                <p className="text-sm font-medium ">{service.Price}</p>
                             </div>
                         </div>
                     ))}
